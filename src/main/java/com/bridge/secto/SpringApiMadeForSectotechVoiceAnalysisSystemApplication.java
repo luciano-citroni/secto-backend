023677f35b70bec800b70c9282c139f43543a4ba
RaviChristian <javier.christian@automining.com.br>
2026-01-08 22:31:36 -0300

    feat: minor adjustments

--- src/main/java/com/bridge/secto/SpringApiMadeForSectotechVoiceAnalysisSystemApplication.java
+++ src/main/java/com/bridge/secto/SpringApiMadeForSectotechVoiceAnalysisSystemApplication.java
@@ -3,13 +3,10 @@ package com.bridge.secto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class SpringApiMadeForSectotechVoiceAnalysisSystemApplication {
 
 	public static void main(String[] args) {
-		Dotenv.configure().ignoreIfMissing().systemProperties().load();
 		SpringApplication.run(SpringApiMadeForSectotechVoiceAnalysisSystemApplication.class, args);
 	}
 
