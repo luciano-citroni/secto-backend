840032f7062ba621ac7c4f56adf9203155b97fec
RaviChristian <javier.christian@automining.com.br>
2026-01-08 22:31:30 -0300

    feat remove dontenv configuration

--- src/main/java/com/bridge/secto/SpringApiMadeForSectotechVoiceAnalysisSystemApplication.java
+++ src/main/java/com/bridge/secto/SpringApiMadeForSectotechVoiceAnalysisSystemApplication.java
@@ -3,10 +3,13 @@ package com.bridge.secto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SpringApiMadeForSectotechVoiceAnalysisSystemApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().ignoreIfMissing().systemProperties().load();
 		SpringApplication.run(SpringApiMadeForSectotechVoiceAnalysisSystemApplication.class, args);
 	}
 
